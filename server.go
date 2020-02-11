11eb2e8f0c987a7c9670863ad204f8a4cbdd9fb5
Will Thomas <will.thomas@roardigital.co.uk>
2020-02-11 14:04:07 +0000

    Autocomplete

--- server.go
+++ server.go
@@ -6,7 +6,8 @@ import (
 	"encoding/json"
 	"encoding/base64"
 	"encoding/csv"
-    "os"
+	"os"
+	"github.com/falmar/goradix"
 )
 
 type JSONResponse struct {
@@ -273,25 +274,36 @@ func ReadCsv(filename string) ([][]string, error) {
 
 func main() {
 	log.Println("Server Starting...")
-	googleLocationSlice := make([]GoogleLocation, 102030)
+	radix := goradix.New(false)
 
 	lines, err := ReadCsv("google_locations.csv")
 	if err != nil {
 		panic(err)
 	}
 	for _, line := range lines {
-        data := GoogleLocation{
-			Criteria_ID: line[0],
-			Name: line[1],
-			Canonical_Name: line[2],
-			Parent_ID: line[3],
-			Country_Code: line[4],
-			Target_Type: line[5],
-			Status: line[6],
-        }
+        // data := GoogleLocation{
+		// 	Criteria_ID: line[0],
+		// 	Name: line[1],
+		// 	Canonical_Name: line[2],
+		// 	Parent_ID: line[3],
+		// 	Country_Code: line[4],
+		// 	Target_Type: line[5],
+		// 	Status: line[6],
+        // }
 		// fmt.Println(data.Name + " " + data.Canonical_Name)
-		googleLocationSlice = append(googleLocationSlice, data)
+		// googleLocationSlice = append(googleLocationSlice, data)
+		radix.Insert(line[2])
 	}
+
+	words, err := radix.AutoComplete("Kab", false)
+
+	if err != nil { // No Match Found
+		return
+	}
+
+	// Output: AutoComplete 'rom'; Words: [manus mane mulus]
+	log.Println("AutoComplete: '%s'; Words: %v\n", "ro", words)
+
 
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
